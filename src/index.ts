8a2c734c26622964f3d828cfafdfc367183f4dbc
Jérémy <3DX900@gmail.com>
2021-10-27 01:09:30 +0200

    ✨ add console log

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,10 @@ import * as http from 'http';
 new Bot();
 
 http.createServer((_, res) => {
-  res.writeHead(200);
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World!');
-}).listen(8080);
+}).listen(4321, () => {
+  console.log('Server running on port 4321');
+});
 
 export default Bot;
