d843cc1368425fdf41db064a332c6b73d8f1430a
Jérémy <3DX900@gmail.com>
2021-10-27 00:57:00 +0200

    ✨ add web server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import 'reflect-metadata';
 import Bot from './Bot';
+import * as http from 'http';
 
 new Bot();
+
+http.createServer((_, res) => {
+  res.writeHead(200);
+  res.end('Hello World!');
+}).listen(8080);
 
 export default Bot;
