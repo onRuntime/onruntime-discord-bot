6995e868254a3b61763f58da02e34072c0d403c3
Jérémy <3DX900@gmail.com>
2021-11-02 12:29:20 +0100

    ⚰️ remove unused http server

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,6 @@
 import 'reflect-metadata';
 import Bot from './Bot';
-import * as http from 'http';
 
 new Bot();
-
-http.createServer((_, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World!');
-}).listen(4321, () => {
-  console.log('Server running on port 4321');
-});
 
 export default Bot;
