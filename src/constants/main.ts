71e457790e377b36c2c3cb510293ce104b74f3cb
Antoine Kingue <etorefr@gmail.com>
2022-10-26 22:38:04 +0200

    🔧 add url to app constants

--- src/constants/main.ts
+++ src/constants/main.ts
@@ -1,6 +1,7 @@
 const APP = {
   NAME: "onRuntime Studio",
   LOGO: "https://cdn.discordapp.com/attachments/719585632313409556/1002202982059868231/onrw.png",
+  URL: "onruntime.com",
 };
 
 export default APP;
