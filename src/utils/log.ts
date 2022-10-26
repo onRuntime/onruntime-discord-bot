b75e369826599bae9656ade305e564b0f1970dc7
Antoine Kingue <etorefr@gmail.com>
2022-10-26 20:00:10 +0200

    🎨 improve log with webhook

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,4 +1,10 @@
 import chalk from "chalk";
+import { MessageBuilder } from "discord-webhook-node";
+import APP from "../constants/main";
+import { hook } from "../services/webhook";
+
+hook.setAvatar(APP.LOGO);
+hook.setUsername("Logs | Expat Facilities");
 
 export const prefixes = {
   wait: chalk.cyan("wait") + "  -",
@@ -9,13 +15,56 @@ export const prefixes = {
   event: chalk.magenta("event") + " -",
 };
 
+const builder = (...message: any[]) =>
+  new MessageBuilder()
+    .setDescription(
+      message
+        .map((m) => (typeof m === "string" ? m : JSON.stringify(m)))
+        .join("\n")
+    )
+    .setFooter(APP.NAME, APP.LOGO)
+    .setTimestamp();
+
+export const embed = {
+  info: (...message: any[]) => builder(...message).setColor(5366244),
+  event: (...message: any[]) => builder(...message).setColor(15753645),
+  ready: (...message: any[]) => builder(...message).setColor(1299607),
+  warn: (...message: any[]) => builder(...message).setColor(16562066),
+  error: (...message: any[]) => builder(...message).setColor(15422583),
+  wait: (...message: any[]) => builder(...message).setColor(5366244),
+};
+
 const Log = {
-  info: (...message: string[]) => console.log(prefixes.info, ...message),
-  warn: (...message: string[]) => console.warn(prefixes.warn, ...message),
-  error: (...message: string[]) => console.error(prefixes.error, ...message),
-  ready: (...message: string[]) => console.log(prefixes.ready, ...message),
-  wait: (...message: string[]) => console.log(prefixes.wait, ...message),
-  event: (...message: string[]) => console.log(prefixes.event, ...message),
+  info: (...message: any[]) => {
+    console.log(prefixes.info, ...message);
+    if (process.env.NODE_ENV === "production")
+      hook.send(embed.info(...message));
+  },
+  warn: (...message: any[]) => {
+    console.warn(prefixes.warn, ...message);
+    if (process.env.NODE_ENV === "production")
+      hook.send(embed.warn(...message));
+  },
+  error: (...message: any[]) => {
+    console.error(prefixes.error, ...message);
+    if (process.env.NODE_ENV === "production")
+      hook.send(embed.error(...message));
+  },
+  ready: (...message: any[]) => {
+    console.log(prefixes.ready, ...message);
+    if (process.env.NODE_ENV === "production")
+      hook.send(embed.ready(...message));
+  },
+  wait: (...message: any[]) => {
+    console.log(prefixes.wait, ...message);
+    if (process.env.NODE_ENV === "production")
+      hook.send(embed.wait(...message));
+  },
+  event: (...message: any[]) => {
+    console.log(prefixes.event, ...message);
+    if (process.env.NODE_ENV === "production")
+      hook.send(embed.event(...message));
+  },
 };
 
 export default Log;
