acf813353ad0c8ad13bb064edcb4ab617857fb11
Antoine Kingue <etorefr@gmail.com>
2022-10-26 23:14:03 +0200

    🩹 fix by adding a ready event

--- src/plugins/worktime.ts
+++ src/plugins/worktime.ts
@@ -2,16 +2,18 @@ import { ChannelType, TextChannel } from "discord.js";
 import CHANNELS from "../constants/channels";
 import { DiscordPlugin } from "../types/plugin";
 
-const WorktimePlugin: DiscordPlugin = async (client) => {
+const WorktimePlugin: DiscordPlugin = (client) => {
   // delete all message from CHANNELS.ONRUNTIME.TEAM.WORKTIME channel on startup, dont forget to check if it's a text channel
-  const channel = await client.channels.cache.get(
-    CHANNELS.ONRUNTIME.TEAM.WORKTIME
-  );
-  if (channel?.type === ChannelType.GuildText) {
-    const textChannel = channel as TextChannel;
-    const messages = await textChannel.messages.fetch();
-    messages.forEach((message) => message.delete());
-  }
+  client.on("ready", async () => {
+    const channel = await client.channels.cache.get(
+      CHANNELS.ONRUNTIME.TEAM.WORKTIME
+    );
+    if (channel?.type === ChannelType.GuildText) {
+      const textChannel = channel as TextChannel;
+      const messages = await textChannel.messages.fetch();
+      messages.forEach(async (message) => await message.delete());
+    }
+  });
 };
 
 export default WorktimePlugin;
