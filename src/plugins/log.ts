af3a376c3351d4db11eaed355e3b5cf9853ef40c
Antoine Kingue <etorefr@gmail.com>
2022-10-26 20:38:29 +0200

    🎨 improve log plugin with message edit handler

--- src/plugins/log.ts
+++ src/plugins/log.ts
@@ -9,6 +9,14 @@ const LogPlugin: DiscordPlugin = (client) => {
       `message deleted by ${message.author?.username}#${message.author?.discriminator} in <#${message.channel.id}> "${message.cleanContent}"`
     );
   });
+
+  // handle when a guild member edit a message
+  client.on("messageUpdate", (oldMessage, newMessage) => {
+    Log.warn(
+      oldMessage.guild?.name,
+      `message edited by ${oldMessage.author?.username}#${oldMessage.author?.discriminator} in <#${oldMessage.channel.id}> "${oldMessage.cleanContent}" -> "${newMessage.cleanContent}"`
+    );
+  });
 };
 
 export default LogPlugin;
