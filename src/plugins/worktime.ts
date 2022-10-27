0cac4a21d6a3c30ae3b27c7223e24dc2432e0549
Antoine Kingue <etorefr@gmail.com>
2022-10-27 16:30:17 +0200

    ✨ add worktime plugin

--- src/plugins/worktime.ts
+++ src/plugins/worktime.ts
@@ -1,6 +1,133 @@
-import { ChannelType, TextChannel } from "discord.js";
+import {
+  ButtonStyle,
+  ChannelType,
+  Client,
+  Collection,
+  Colors,
+  Events,
+  GuildMember,
+  TextChannel,
+} from "discord.js";
 import CHANNELS from "../constants/channels";
+import APP from "../constants/main";
 import { DiscordPlugin } from "../types/plugin";
+import daysjs from "dayjs";
+import Log from "../utils/log";
+import Worktime from "../models/Worktime";
+import schedule from "node-schedule";
+
+export const isInWorkVoiceChannel = (
+  client: Client<boolean>,
+  userId: string | undefined
+) => {
+  // check all voice channels which a name that contain "work" from each guilds to see if a user with same user id is present
+  // return true if the user is present in a voice channel
+  // return false if the user is not present in a voice channel
+  let isInWorkVoiceChannel = false;
+  const guilds = client.guilds.cache;
+  guilds.forEach((guild) => {
+    const voiceChannels = guild.channels.cache.filter(
+      (channel) =>
+        channel.name.toLowerCase().includes("work") &&
+        channel.type === ChannelType.GuildVoice
+    );
+    voiceChannels.forEach((channel) => {
+      const members = channel.members as Collection<string, GuildMember>;
+      members.forEach((member) => {
+        if (member.user.id === userId) {
+          isInWorkVoiceChannel = true;
+        }
+      });
+    });
+  });
+  return isInWorkVoiceChannel;
+};
+
+const startWorktime = async (client: Client, userId: string | undefined) => {
+  if (!userId) return;
+
+  const worktime = await Worktime.findOne({
+    userId: userId,
+    endAt: null,
+  });
+
+  const user = await client.users.fetch(userId);
+
+  if (worktime) {
+    // if the user has already started his worktime, send a message to the user
+    user.send(
+      `❌ - Vous avez déjà commencé votre service à ${daysjs(
+        worktime.startAt
+      ).format("HH:mm")}`
+    );
+  } else {
+    // add new Wortime with only the startAt
+    await Worktime.create({
+      startAt: new Date(),
+      userId: userId,
+    });
+
+    user.send(
+      `✅ - Votre prise de service a été validée à ${daysjs().format("HH:mm")}`
+    );
+    Log.info(
+      `✅ - Prise de service validée à ${daysjs().format("HH:mm")} par ${user}`
+    );
+  }
+};
+
+const endWorktime = async (client: Client, userId: string | undefined) => {
+  if (!userId) return;
+
+  const worktime = await Worktime.findOne({
+    userId: userId,
+    endAt: null,
+  });
+
+  const user = await client.users.fetch(userId);
+
+  if (worktime) {
+    // if the user has already started his worktime, end it
+    worktime.endAt = new Date();
+    await worktime.save();
+
+    // get all worktimes from the userId and tell him how many time he spent
+    const worktimes = await Worktime.find({
+      userId: userId,
+    });
+
+    let totalWorktime = 0;
+    worktimes.forEach((worktime) => {
+      if (worktime.startAt && worktime.endAt) {
+        totalWorktime += worktime.endAt.getTime() - worktime.startAt.getTime();
+      }
+    });
+
+    user.send(
+      `✅ - Votre fin de service a été validée à ${daysjs().format(
+        "HH:mm"
+      )} - Vous avez passé ${Math.floor(
+        totalWorktime / 1000 / 60 / 60
+      )}h${Math.floor(
+        (totalWorktime / 1000 / 60) % 60
+      )}min à travailler cette semaine`
+    );
+    Log.info(
+      `✅ - Fin de service validée à ${daysjs().format(
+        "HH:mm"
+      )} par ${user} - ${Math.floor(
+        totalWorktime / 1000 / 60 / 60
+      )}h${Math.floor((totalWorktime / 1000 / 60) % 60)}min`
+    );
+
+    return true;
+  } else {
+    // if the user has not started his worktime, send a message to the user
+    user.send("❌ Vous n'avez pas commencé votre service aujourd'hui");
+
+    return false;
+  }
+};
 
 const WorktimePlugin: DiscordPlugin = (client) => {
   // delete all message from CHANNELS.ONRUNTIME.TEAM.WORKTIME channel on startup, dont forget to check if it's a text channel
@@ -12,7 +139,182 @@ const WorktimePlugin: DiscordPlugin = (client) => {
       const textChannel = channel as TextChannel;
       const messages = await textChannel.messages.fetch();
       messages.forEach(async (message) => await message.delete());
+
+      // worktime sert a pointé les heures des membres de l'équipe
+      // appuyez sur le bouton prise de service pour pointer votre arrivée
+      // puis fin de service pour pointer votre départ
+      // veillez a bien vous connecter à un salon vocal pour que votre prise de service soit bien prise en compte
+      // send this message to CHANNELS.ONRUNTIME.TEAM.WORKTIME channel as an embed message
+
+      const instructionEmbed = {
+        color: Colors.White,
+        title: "Worktime (Beta)",
+        description:
+          "Pointage des heures des membres de l'équipe\n\n" +
+          "**Prise de service**\n" +
+          "Appuyez sur le bouton prise de service pour pointer votre arrivée\n\n" +
+          "**Fin de service**\n" +
+          "Appuyez sur le bouton fin de service pour pointer votre départ\n\n" +
+          "**Attention**\n" +
+          "Veillez à bien vous connecter à un salon vocal **Work** pour que votre prise de service soit bien prise en compte",
+        footer: {
+          text: `Merci à vous et bon courage - ${APP.NAME}`,
+          icon_url: APP.LOGO,
+        },
+      };
+
+      // add buttons to the embed message
+      textChannel.send({
+        embeds: [instructionEmbed],
+        components: [
+          {
+            type: 1,
+            components: [
+              {
+                type: 2,
+                style: ButtonStyle.Primary,
+                label: "✨ Prise de service",
+                custom_id: "worktime_start",
+              },
+              {
+                type: 2,
+                style: ButtonStyle.Danger,
+                label: "🚪 Fin de service",
+                custom_id: "worktime_end",
+              },
+            ],
+          },
+        ],
+      });
     }
+  });
+
+  client.on(Events.InteractionCreate, async (interaction) => {
+    if (!interaction.isButton()) return;
+
+    // check if the button has the custom id worktime_start
+    switch (interaction.customId) {
+      case "worktime_start":
+        // validate interaction and delete
+        if (isInWorkVoiceChannel(client, interaction.user.id)) {
+          //interaction.reply("✅ Prise de service validée");
+          interaction.deferReply();
+
+          startWorktime(client, interaction.user.id);
+
+          interaction.deleteReply();
+        } else {
+          interaction.reply(
+            "❌ - Vous devez être connecté à un salon vocal **Work**"
+          );
+        }
+
+        break;
+
+      case "worktime_end":
+        interaction.deferReply();
+
+        endWorktime(client, interaction.user.id);
+
+        interaction.deleteReply();
+        break;
+    }
+  });
+
+  // if a user which as started his worktime disconnect from the voice channel more than 10 minutes, end his worktime
+  client.on(Events.VoiceStateUpdate, async (oldState, newState) => {
+    if (
+      oldState.channelId &&
+      !newState.channelId &&
+      oldState.channel?.name.toLowerCase().includes("work")
+    ) {
+      const worktime = await Worktime.findOne({
+        userId: oldState.member?.user.id,
+        endAt: null,
+      });
+
+      if (worktime) {
+        const diff = daysjs().diff(daysjs(worktime.startAt), "minute");
+
+        if (diff > 10) {
+          endWorktime(client, oldState.member?.user.id);
+        }
+      }
+    }
+  });
+
+  // every sunday at midday, send a leaderboard in CHANNELS.ONRUNTIME.TEAM.LEADERBOARD
+  schedule.scheduleJob("0 12 * * 0", async () => {
+    // schedule.scheduleJob("* * * * *", async () => {
+    // end everyone worktime
+    const inProgressWorktimes = await Worktime.find({
+      endAt: null,
+    });
+
+    inProgressWorktimes.forEach(async (worktime) => {
+      await endWorktime(client, worktime.userId);
+    });
+
+    // get all worktimes
+    const worktimes = await Worktime.find();
+
+    // create a map with the total worktime of each user
+    const worktimeMap = new Map<string, number>();
+    worktimes.forEach((worktime) => {
+      const totalWorktime = worktimeMap.get(worktime.userId) || 0;
+      worktimeMap.set(
+        worktime.userId,
+        totalWorktime + daysjs(worktime.endAt).diff(daysjs(worktime.startAt))
+      );
+    });
+
+    // sort the map by total worktime
+    const sortedWorktimeMap = new Map(
+      [...worktimeMap.entries()].sort((a, b) => b[1] - a[1])
+    );
+
+    // create the leaderboard embed
+    const leaderboardEmbed = {
+      color: Colors.White,
+      title: "Leaderboard (Beta)",
+      description:
+        `Voici le classement des membres de l'équipe pour la semaine du ${daysjs()
+          .startOf("week")
+          .format("DD/MM/YYYY")} au ${daysjs()
+          .endOf("week")
+          .format("DD/MM/YYYY")}\n\n` +
+        Array.from(sortedWorktimeMap.entries())
+          .map(([userId, totalWorktime], index) => {
+            const user = client.users.cache.get(userId);
+            return `${index + 1}. ${Math.floor(
+              totalWorktime / 1000 / 60 / 60
+            )}h ${Math.floor((totalWorktime / 1000 / 60) % 60)}min - **${
+              user ? user.username : "Utilisateur inconnu"
+            }**`;
+          })
+          .join("\n"),
+
+      footer: {
+        text: APP.NAME,
+        icon_url: APP.LOGO,
+      },
+    };
+
+    // send the leaderboard embed to CHANNELS.ONRUNTIME.TEAM.LEADERBOARD
+    const channel = client.channels.cache.get(
+      CHANNELS.ONRUNTIME.TEAM.LEADERBOARD
+    );
+
+    // if channel is guildtext channel
+    if (channel && channel.type === ChannelType.GuildText) {
+      const textChannel = channel as TextChannel;
+
+      // send the leaderboard embed
+      textChannel.send({ embeds: [leaderboardEmbed] });
+    }
+
+    // remove all worktimes from the database
+    await Worktime.deleteMany({});
   });
 };
 
