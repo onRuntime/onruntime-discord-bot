498557b5ece7b2befb78c70e8b42a914e15decb9
Antoine Kingue <etorefr@gmail.com>
2022-12-18 15:19:55 +0100

    🔧 add timezone to constants

--- src/constants/main.ts
+++ src/constants/main.ts
@@ -2,6 +2,7 @@ const APP = {
   NAME: "onRuntime Studio",
   LOGO: "https://cdn.discordapp.com/attachments/719585632313409556/1002202982059868231/onrw.png",
   URL: "onruntime.com",
+  TIMEZONE: "Europe/Paris",
 };
 
 export default APP;
