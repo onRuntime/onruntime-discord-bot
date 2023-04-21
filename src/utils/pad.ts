4384b63ebdb5203e0369e04123233428daad381e
Antoine Kingue <etorefr@gmail.com>
2023-04-21 19:04:22 +0200

    🔥 remove utils

--- src/utils/pad.ts
+++
@@ -1,7 +0,0 @@
-const pad = (num: number, size: number) => {
-  let s = num + "";
-  while (s.length < size) s = "0" + s;
-  return s;
-};
-
-export default pad;
--- src/utils/reduceString.ts
+++
@@ -1,8 +0,0 @@
-const reduceString = (str: string, length: number) => {
-  if (str.length > length) {
-    return str.substring(0, length - 3) + "...";
-  }
-  return str;
-};
-
-export default reduceString;
