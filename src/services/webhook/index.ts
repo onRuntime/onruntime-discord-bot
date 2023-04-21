f010b3c32a7731d1960875d557c30a395681664b
Antoine Kingue <etorefr@gmail.com>
2023-04-21 19:03:58 +0200

    🔥 remove services

--- src/services/webhook/index.ts
+++
@@ -1,8 +0,0 @@
-import { Webhook } from "discord-webhook-node";
-import dotenv from "dotenv-flow";
-
-dotenv.config({
-  silent: true,
-});
-
-export const hook = new Webhook(process.env.DISCORD_WEBHOOK_URL || "");
