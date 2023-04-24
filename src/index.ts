d33ac5088eac4cbb6f5af9a291d602d485af39ab
Antoine Kingue <etorefr@gmail.com>
2023-04-24 04:34:40 +0200

    🎨 add dixt plugin affix to main function

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import dixt from "dixt";
 import dixtPluginLogs from "dixt-plugin-logs";
+import dixtPluginAffix from "dixt-plugin-affix";
 import dixtPluginPresence from "dixt-plugin-presence";
 import dixtPluginReact from "dixt-plugin-react";
 import dixtPluginTwitch from "dixt-plugin-twitch";
 import dixtPluginPresenceOptions from "./options/presence";
 import dixtPluginReactOptions from "./options/react";
 import dixtPluginTwitchOptions from "./options/twitch";
+import dixtPluginAffixOptions from "./options/affix";
 
 const main = async () => {
   const instance = new dixt({
@@ -15,6 +17,7 @@ const main = async () => {
     },
     plugins: [
       dixtPluginLogs,
+      [dixtPluginAffix, dixtPluginAffixOptions],
       [dixtPluginReact, dixtPluginReactOptions],
       [dixtPluginPresence, dixtPluginPresenceOptions],
       [dixtPluginTwitch, dixtPluginTwitchOptions],
