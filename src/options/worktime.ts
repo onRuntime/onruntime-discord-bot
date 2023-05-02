f675ab7b2a0dc9e7d9eb1f35f094d5ffd7c070ca
Antoine Kingue <etorefr@gmail.com>
2023-05-02 05:38:00 +0200

    ✨ add worktime plugin

---
+++ src/options/worktime.ts
@@ -0,0 +1,19 @@
+import { DixtPluginWorktimeOptions } from "dixt-plugin-worktime";
+
+import CHANNELS from "../constants/channels";
+import ROLES from "../constants/roles";
+
+const DixtPluginWorktimeOptions: DixtPluginWorktimeOptions = {
+  channels: {
+    main: [CHANNELS.ONRUNTIME.TEAM.INFORMATION.WORKTIME],
+    leaderboard: CHANNELS.ONRUNTIME.TEAM.INFORMATION.LEADERBOARD,
+    workChannelNames: ["Work", "Meeting"],
+  },
+  quotas: {
+    [ROLES.ONRUNTIME.EMPLOYEE]: 30,
+    [ROLES.ONRUNTIME.MEMBER]: 6,
+    [ROLES.ONRUNTIME.TRAINEE]: 20,
+  },
+};
+
+export default DixtPluginWorktimeOptions;
