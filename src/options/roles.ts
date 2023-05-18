7da092d30bd8c3b6972b683f0b1b5305c4618c4f
Antoine Kingue <etorefr@gmail.com>
2023-05-18 20:41:04 +0200

    🔧 update roles options

--- src/options/roles.ts
+++ src/options/roles.ts
@@ -3,7 +3,7 @@ import { DixtPluginRolesOptions } from "dixt-plugin-roles";
 import CHANNELS from "../constants/channels";
 import ROLES from "../constants/roles";
 
-const dixtPluginTwitchOptions: DixtPluginRolesOptions = {
+const dixtPluginRolesOptions: DixtPluginRolesOptions = {
   channels: [
     {
       id: CHANNELS.TONIGHTPASS.INFORMATIONS.ROLES,
@@ -15,10 +15,11 @@ const dixtPluginTwitchOptions: DixtPluginRolesOptions = {
           emoji: "🧪",
           description:
             "You would like to join our Beta test program, and receive instructions on how to do so, give us feedback and help us develop the application!",
+          message: `Welcome to our Beta test program!\n\nIf you use iOS:\n- [Follow the instructions by clicking here](https://testflight.apple.com/join/ZtOgXCpb)\n- Give us your feedback in <#${CHANNELS.TONIGHTPASS.APPS.IOS_BETA}>\n\nIf you are using Android:\n*It's not available yet but it's coming soon!*`,
         },
       ],
     },
   ],
 };
 
-export default dixtPluginTwitchOptions;
+export default dixtPluginRolesOptions;
