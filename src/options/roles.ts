146b690b8ef9bc6e6632f2ad49e90074ba6f22d7
Antoine Kingue <contact@antoinek.fr>
2024-11-23 15:24:01 +0100

    ✏️ update beta test program instructions

--- src/options/roles.ts
+++ src/options/roles.ts
@@ -15,7 +15,8 @@ const dixtPluginRolesOptions: DixtPluginRolesOptions = {
           emoji: "🧪",
           description:
             "You would like to join our Beta test program, and receive instructions on how to do so, give us feedback and help us develop the application!",
-          message: `Welcome to our Beta test program!\n\nIf you use iOS:\n- [Follow the instructions by clicking here](https://testflight.apple.com/join/ZtOgXCpb)\n- Give us your feedback in <#${CHANNELS.TONIGHTPASS.APPS.IOS_BETA}>\n\nIf you are using Android:\n*It's not available yet but it's coming soon!*`,
+          // message: `Welcome to our Beta test program!\n\nIf you use iOS:\n- [Follow the instructions by clicking here](https://testflight.apple.com/join/ZtOgXCpb)\n- Give us your feedback in <#${CHANNELS.TONIGHTPASS.APPS.IOS_BETA}>\n\nIf you are using Android:\n*It's not available yet but it's coming soon!*`,
+          message: `Welcome to our Beta test program!\n\nTo access our beta:\n1. Go to https://staging.tonightpass.com\n2. Enter the password: \`beta2024\`\n3. Give us your feedback in <#${CHANNELS.TONIGHTPASS.APPS.IOS_BETA}>`,
         },
       ],
     },
